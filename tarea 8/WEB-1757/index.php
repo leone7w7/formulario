4767d9b9767beea6b61ee8657456e5a0ad12b2dd
leone 7w7 <142859906+leone7w7@users.noreply.github.com>
2023-10-09 22:49:34 -0600

    Add files via upload

---
+++ tarea 8/WEB-1757/index.php
@@ -0,0 +1,26 @@
+<?php
+    $edad =29;
+
+    echo "<h1>Mi edad es:" .$edad."/<h1>"
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="media/img/favicon.ico" type="image/x-icon">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <section>
+            <div>
+
+            </div>
+        </section>
+    </main>
+</body>
+</html>
